371848a0f20f800fa023cb22c2397af80b4c6c69
Léo <ldos_rj2008@hotmail.com>
2021-09-29 04:21:31 -0300

    Aula 10.

---
+++ src/NerdStore.Pagamentos.Business/IPagamentoService.cs
@@ -0,0 +1,11 @@
+﻿using NerdStore.Core.DomainObjects.Dto;
+using System.Threading.Tasks;
+
+namespace NerdStore.Pagamentos.Business
+{
+    public interface IPagamentoService
+    {
+        Task<Transacao> RealizarPagamentoPedido(PagamentoPedido pagamentoPedido);
+    }
+}
+
